86d86d9be1ea65d938d75b6f08459cee8d19bbe0
LeiRabe <leiya.rabe@gmail.com>
2020-06-10 19:31:24 +0200

    Adding a function to concat strings

--- src/main/java/com/dant/utils/StringConcat.java
+++ src/main/java/com/dant/utils/StringConcat.java
@@ -1,4 +1,13 @@
 package com.dant.utils;
 
+import java.util.ArrayList;
+
 public class StringConcat {
+    public static String concatString(ArrayList<Integer> positions, ArrayList<String> line){
+        String s ="";
+        for (int i:positions) {
+                s += line.get(i);
+        }
+        return s;
+    }
 }
