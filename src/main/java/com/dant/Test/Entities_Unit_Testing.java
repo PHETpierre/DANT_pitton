8d32bddbf0a358d2416ebc8cfd355e7ec4ee0c24
LeiRabe <leiya.rabe@gmail.com>
2020-06-10 19:21:05 +0200

    Adding new dependency

--- src/main/java/com/dant/Test/Entities_Unit_Testing.java
+++ src/main/java/com/dant/Test/Entities_Unit_Testing.java
@@ -4,7 +4,6 @@ import com.dant.entity.DataBase;
 import junit.framework.*;
 import com.dant.entity.Column;
 import com.dant.entity.Table;
-import org.joda.time.DateTime;
 
 import java.util.ArrayList;
 
@@ -14,13 +13,15 @@ public class Table_Test extends TestCase{
         Table table1 = new Table("Taxi1");
         Column col1 = new Column("VendorID","String");
         Column col2 = new Column("tpep_pickup_datetime", "DateTime");
-        ArrayList<Column> cols = new ArrayList<Column>();
+        ArrayList<Column> cols = new ArrayList<>();
         cols.add(col1);
         cols.add(col2);
         table1.setColumns(cols);
 
-        //expect the table name to be the given
-        assertEquals("Taxi1",table1.getName());
+        //expect the table name to be the given and the table's columns to be created and usable 
+        assertEquals("Can't get the table name","Taxi1",table1.getName());
+        assertEquals("Can't get the columns",2,table1.getColumns().size());
+        assertEquals("Can't get the chosen column name","VendorID",table1.getColumns().get(0).getName());
     }
 
 }
